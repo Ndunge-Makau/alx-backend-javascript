519aea725a9e43aeec0da54f56b1fcf0d29e1868
Ndunge-Makau <winniemakau@outloo.com>
2025-01-30 15:35:15 +0300

    Fix 0-get_list_students.js.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-export default function getListStudents() {
+export default function getListStudents () {
   return [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
