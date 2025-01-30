0df6387919ba82622711b2fcf17ebc043af1eedb
Ndunge-Makau <winniemakau@outloo.com>
2025-01-30 17:05:25 +0300

    Add files.

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(studentData) {
+  const numberOfStudents = studentData.reduce(
+    (cumulative, currentValue) => currentValue.id + cumulative,
+    0
+  );
+  return numberOfStudents;
+}
