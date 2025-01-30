b6b89e67daeb953e9dc9a86906a0f88555b4a8ac
Ndunge-Makau <winniemakau@outloo.com>
2025-01-30 17:08:58 +0300

    Fix 3-get_ids_sum.js.

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(studentData) {
-  const numberOfStudents = studentData.reduce(
-    (cumulative, currentValue) => currentValue.id + cumulative,
-    0
-  );
-  return numberOfStudents;
+	const numberOfStudents = studentData.reduce(
+		(cumulative, currentValue) => currentValue.id + cumulative,
+		0
+	);
+	return numberOfStudents;
 }
